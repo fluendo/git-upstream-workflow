ea128ef6ae609271576165f3c5e4f85baad7c111
Jorge Zapata <jorgeluis.zapata@gmail.com>
2024-06-11 02:10:05 +0200

    General improvements
    Generate proper logger exceptions
    Exit code != 0 on error
    Keep a backup branch
    Push each modified branch

--- guw/main.py
+++ guw/main.py
@@ -1,5 +1,6 @@
 import argparse
 import colorlog
+from datetime import date
 import git
 import logging
 import os
@@ -21,8 +22,10 @@ class GUW:
     def __init__(self, config):
         self.config = config
 
-    def _sync_at(self, tmpdir, backup):
+    def _sync_at(self, tmpdir, backup, local):
         logger.info(f"Work directory at {tmpdir}")
+        today = str(date.today())
+        to_push = []
 
         # Fetch the source branch
         source_remote = [x["url"] for x in self.config["remotes"] if x["name"] == self.config["source"]["remote"]][0]
@@ -45,8 +48,9 @@ class GUW:
         for feature in self.config["features"]:
             logger.info(f"Syncing feature {feature['name']} with previous active {prev_active_feature['name']}")
             # Checkout the remote branch
-            logger.debug(f"Creating local branch {feature['remote']}/{feature['name']}")
-            repo.git.checkout("-b", feature["name"], "{}/{}".format(feature["remote"], feature["name"]))
+            feature_branch = f"{feature['remote']}/{feature['name']}"
+            logger.debug(f"Creating local branch {feature_branch}")
+            repo.git.checkout("-b", feature["name"], feature_branch)
             # Check the status to know how to proceed
             if feature["status"] == "integrated":
                 if has_pending:
@@ -56,34 +60,43 @@ class GUW:
             elif feature["status"] == "merged":
                 # When a feature (feature1) is merged, we don't really know what commits went upstream
                 # but we do know that the following feature (feature2) should only apply the commits
-                # found on feature2 and not in feature1.
+                # found on feature2 and not in feature1. This will make the feature1 to be in state
+                # integrated afterwards
                 logger.debug(f"Feature {feature['name']} already merged nothing to do")
                 prev_active_feature = feature
             elif feature["status"] == "merging" or feature["status"] == "pending":
-                logger.debug(f"Rebasing {feature['name']} onto {prev_active_feature['name']} until {prev_feature['remote']}/{prev_feature['name']}")
-                # Checkout the feature locally
+                prev_feature_branch = f"{prev_feature['remote']}/{prev_feature['name']}"
+                logger.debug(f"Rebasing {feature['name']} onto {prev_active_feature['name']} until {prev_feature_branch}")
+                if backup:
+                    feature_backup_name = f"{feature['name']}-{today}"
+                    logger.debug(f"Backing up {feature['name']} into {feature_backup_name}")
+                    repo.git.branch("-c", feature_backup_name)
+                    to_push.append((feature_backup_name, feature['remote']))
                 # Ok, let's rebase on top of the prev_active_feature
-                repo.git.rebase("--onto", prev_active_feature["name"], f"{prev_feature['remote']}/{prev_feature['name']}", feature["name"])
+                repo.git.rebase("--onto", prev_active_feature["name"], prev_feature_branch, feature["name"])
                 prev_active_feature = feature
                 has_pending = True
             prev_feature = feature
+        # Push every branch
+        if not local:
+            for branch,remote in to_push:
+                repo.git.push(remote, branch)
+        # TODO generate a new .toml for features from merged to integrated
 
-        # apply each diff of a non merged branch on top of the current aggregated branch
-        # Depending on the status, proceed accordingly
-        # with --backup, the original branches are kept on branches suffixed by a date
-        # generate a new .toml with the changes
-
-    def sync(self, backup=True, keep=False):
+    def sync(self, backup, keep, local):
         tmpdir = tempfile.mkdtemp()
         exception = None
         try:
-            self._sync_at(tmpdir, backup)
+            self._sync_at(tmpdir, backup, local)
         except git.exc.GitCommandError as e:
             exception = e
         if not keep:
             shutil.rmtree(tmpdir)
         if exception:
-            raise exception
+            logger.error(f"Command failed: {' '.join(exception.command)}")
+            print(exception.stdout, file=sys.stdout)
+            print(exception.stderr, file=sys.stderr)
+            exit(1)
 
     def markdown(self):
         # generate the markup which is something like
@@ -115,6 +128,7 @@ def run():
     sync_args = subparser.add_parser("sync", help="Sync the list of branches based on the configuration")
     sync_args.add_argument("-b", "--backup", help="Generate backup branches", action="store_true")
     sync_args.add_argument("-k", "--keep", help="Keep temporary folder", action="store_true")
+    sync_args.add_argument("-l", "--local", help="Don't push anything, but keep everything local", action="store_true")
     # Markdown subcommand
     markdown_args = subparser.add_parser("markdown", help="Create a markdown content")
 
@@ -128,6 +142,6 @@ def run():
         config = tomli.load(fconfig)
         guw = GUW(config)
         if args.command == "sync":
-            guw.sync(args.backup, args.keep)
+            guw.sync(args.backup, args.keep, args.local)
         elif args.command == "markdown":
             guw.markdown()
